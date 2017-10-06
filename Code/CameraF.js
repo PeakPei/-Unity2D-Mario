368a830e9e0e44229cd9f125de87e5d86f0fe654
naluebet manpati <naluebet.m@gmail.com>
2017-10-06 16:32:58 +0700

    updeta
    updeta 061017

---
+++ Code/CameraF.js
@@ -0,0 +1,12 @@
+﻿var Player : Transform;
+var DistanceFromPlayer : float = 5;
+var StaticCameraY : float = 1;
+
+function Update (){
+	transform.position.z = Player.position.z - DistanceFromPlayer;
+	transform.position.x = Player.position.x + 3;
+}
+
+function LateUpdate (){
+	GetComponent.<Camera>().main.transform.position.y = StaticCameraY;
+}
---
+++ Code/playerW.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerW : MonoBehaviour {
+	
+	void Update () {
+		if (gameObject.transform.position.y < -7){
+			Die();
+		}
+	}
+
+	void Die (){
+		SceneManager.LoadScene ("mario_1");
+	}
+}
